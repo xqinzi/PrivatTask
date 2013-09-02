38b25ffd989932e7eed2fdf7e8e3fc61343acb66
AndrewMalapura <malapura@mail.ru>
2013-09-02 23:35:02 +0300

    22

--- PrivatTask/src/main/java/org/andrew/malapura/dao/PersonalAccountDAO.java
+++ PrivatTask/src/main/java/org/andrew/malapura/dao/PersonalAccountDAO.java
@@ -1,7 +1,7 @@
 package org.andrew.malapura.dao;
 
-import org.andrew.malapura.entity.PersonalAccount;
+import org.andrew.malapura.entity.PersonAccount;
 
-public interface PersonalAccountDAO extends GeneralizedDAO<PersonalAccount>{
+public interface PersonalAccountDAO extends GeneralizedDAO<PersonAccount>{
 
 }
